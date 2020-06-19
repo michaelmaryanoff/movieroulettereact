ed4b6bd531ad965904807cb902d4b86994ff178d
MichaelMaryanoff <mmaryanoff@gmail.com>
2020-06-19 15:32:35 -0500

    Set up field with redux form

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,20 +1,55 @@
 import React from 'react';
+import reelLogo from '../../src/images/reel-logo.png';
+import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
+
+import InputField from './InputField';
 
 class LoginForm extends React.Component {
-    renderLoginForm() {
-        return (
-            <div className="column">
-                <h2 className="ui blue image header">
-                    <div className="content">
-                        Log in to your TMDB Account
-                    </div>
-                </h2>
-            </div>
-        )
-    }
+  renderField = () => {
+    return <div>email</div>;
+  };
+
+  renderLoginForm() {
+    return (
+      <div className="column">
+        <h2 className="ui image header">
+          <img src={reelLogo} className="image" alt="logo" />
+          <div className="content">Log in to your TMDB Account</div>
+        </h2>
+        <form className="ui large form">
+          <div className="ui stacked segment">
+            <Field
+              name="login"
+              className="field"
+              component={component => <InputField fieldType="username" />}
+            />
+            <Field
+              name="login"
+              className="field"
+              component={component => <InputField fieldType="password" />}
+            />
+            <button className="ui fuild large submit button">Login</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
   render() {
-    return <div>LoginForm</div>;
+    return (
+      <div className="ui container">
+        <div className="ui middle aligned center aligned grid">{this.renderLoginForm()}</div>
+      </div>
+    );
   }
 }
 
-export default LoginForm;
+const mapStateToProps = state => {
+  return state;
+};
+
+// export default connect(reduxForm(mapStateToProps, { form: 'loginForm' })(LoginForm));
+const formWrapped = reduxForm({ form: 'loginForm' })(LoginForm);
+
+export default connect(mapStateToProps)(withRouter(formWrapped));
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
 import authReducer from './authReducer';
 import spinReducer from './spinReducer';
+import { reducer as formReducer } from 'redux-form';
 
-export default combineReducers({ auth: authReducer, spin: spinReducer });
+export default combineReducers({ auth: authReducer, spin: spinReducer, form: formReducer });
