a1e4aa69f5779cc8725c955fb6f0f2b100c6cbae
MichaelMaryanoff <mmaryanoff@gmail.com>
2020-06-30 15:19:16 -0500

    Generate an array of years

--- src/components/SpinPage.js
+++ src/components/SpinPage.js
@@ -1,8 +1,23 @@
 import React from 'react';
 
 class SpinPage extends React.Component {
+  generateYearArray() {
+    let currentYear = new Date().getFullYear();
+    let firstYear = 1920;
+    let years = [];
+
+    for (let i = firstYear; i <= currentYear; i++) {
+      years.push(i);
+    }
+    return years;
+  }
+
+  renderSpinForm() {
+    return <div>Spin Form</div>;
+  }
+
   render() {
-    return <div>SpinPage</div>;
+    return <div>{this.renderSpinForm()}</div>;
   }
 }
 
