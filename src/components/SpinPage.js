2df3d7cce3c7d1c42652be5b1e12a47c57516710
MichaelMaryanoff <mmaryanoff@gmail.com>
2020-06-30 17:15:38 -0500

    Populate form with years and rating

--- src/components/SpinPage.js
+++ src/components/SpinPage.js
@@ -1,6 +1,13 @@
 import React from 'react';
 
 class SpinPage extends React.Component {
+  constructor(props) {
+    super(props);
+    const yearArray = this.generateYearArray();
+
+    this.state = { yearArray, yearFrom: yearArray[0], yearTo: yearArray[yearArray.length - 1] };
+  }
+
   generateYearArray() {
     let currentYear = new Date().getFullYear();
     let firstYear = 1920;
@@ -11,13 +18,83 @@ class SpinPage extends React.Component {
     }
     return years;
   }
+  renderDropDown(type) {
+    // Creates a zero indexed array for ratings
+    const ratingArray = Array.from(new Array(10), (i, index) => index);
+
+    if (type === 'yearFrom') {
+      return this.state.yearArray.map(year => {
+        return (
+          <option key={year} value={year}>
+            {year}
+          </option>
+        );
+      });
+    }
+
+    if (type === 'yearTo') {
+      return this.state.yearArray.map(year => {
+        return (
+          <option key={year} value={year}>
+            {year}
+          </option>
+        );
+      });
+    }
+    if (type === 'minimumRating') {
+      return ratingArray.map(rating => {
+        let displayRating = (rating + 1) * 10;
+        return (
+          <option key={displayRating} value={displayRating}>
+            {displayRating}%
+          </option>
+        );
+      });
+    }
+  }
 
   renderSpinForm() {
-    return <div>Spin Form</div>;
+    return (
+      <div className="pusher">
+        <div className="ui middle aligned center aligned grid">
+          <div className="two column row">
+            <div className="column">
+              <form className="ui large form error">
+                <div className="ui segment">
+                  <h2 className="ui teal image header">
+                    <div className="content">Find a movie!</div>
+                  </h2>
+                  <div className="fields">
+                    <div className="field">
+                      <label>From</label>
+                      <select name="yearFrom" className="ui dropdown">
+                        {this.renderDropDown('yearFrom')}
+                      </select>
+                    </div>
+                    <div className="field">
+                      <label>To</label>
+                      <select name="yearTo" className="ui dropdown">
+                        {this.renderDropDown('yearTo')}
+                      </select>
+                    </div>
+                    <div className="field">
+                      <label>Minimum Rating</label>
+                      <select name="Minimum Rating" className="ui dropdown">
+                        {this.renderDropDown('minimumRating')}
+                      </select>
+                    </div>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 
   render() {
-    return <div>{this.renderSpinForm()}</div>;
+    return <div className="ui container">{this.renderSpinForm()}</div>;
   }
 }
 
