fa77592e1eb973578f39466b039d79412871741a
MichaelMaryanoff <mmaryanoff@gmail.com>
2020-07-27 16:51:12 -0500

    Break out Watchlist Card into component

--- src/components/WatchList.js
+++ src/components/WatchList.js
@@ -2,41 +2,11 @@ import React from 'react';
 import { withRouter } from 'react-router';
 import { connect } from 'react-redux';
 
-import reelLogoPlaceHolder from '../images/ReelLogoPlaceholder.jpg';
+import WatchlistCard from './WatchlistCard';
 
 class WatchList extends React.Component {
-  renderWatchList() {
-    if (this.props.watchList) {
-      return this.props.watchList.results.map(movie => {
-        let imageURL = `https://image.tmdb.org/t/p/original/${movie.poster_path}`;
-
-        if (!movie.poster_path) {
-          imageURL = reelLogoPlaceHolder;
-        }
-
-        return (
-          <div className="ui card" key={`${movie.id}`}>
-            <div className="image">
-              <img src={imageURL} alt={movie.id} />
-            </div>
-            <div className="content">
-              <div className="header">{movie.original_title}</div>
-              <div className="meta">Released: {movie.release_date}</div>
-              <div className="description">{movie.overview}</div>
-              <div className="extra content">Average Score:{movie.vote_average}</div>
-            </div>
-          </div>
-        );
-      });
-    }
-    return (
-      <div className="ui center aligned segment">
-        <h3>You must be logged in to access this page.</h3>
-      </div>
-    );
-  }
   render() {
-    return <div>{this.renderWatchList()}</div>;
+    return <WatchlistCard />;
   }
 }
 
---
+++ src/components/WatchlistCard.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { withRouter } from 'react-router';
+import { connect } from 'react-redux';
+import reelLogoPlaceHolder from '../images/ReelLogoPlaceholder.jpg';
+
+class WatchlistCard extends React.Component {
+  renderWatchList() {
+    if (this.props.watchList) {
+      return this.props.watchList.results.map(movie => {
+        let imageURL = `https://image.tmdb.org/t/p/original/${movie.poster_path}`;
+
+        if (!movie.poster_path) {
+          imageURL = reelLogoPlaceHolder;
+        }
+
+        return (
+          <div className="ui card" key={`${movie.id}`}>
+            <div className="image">
+              <img src={imageURL} alt={movie.id} />
+            </div>
+            <div className="content">
+              <div className="header">{movie.original_title}</div>
+              <div className="meta">Released: {movie.release_date}</div>
+              <div className="description">{movie.overview}</div>
+              <div className="extra content">Average Score:{movie.vote_average}</div>
+            </div>
+          </div>
+        );
+      });
+    }
+    return (
+      <div className="ui center aligned segment">
+        <h3>You must be logged in to access this page.</h3>
+      </div>
+    );
+  }
+  render() {
+    console.log('props wlc', this.props);
+
+    return <div>{this.renderWatchList()}</div>;
+  }
+}
+
+const mapStateToProps = state => {
+  console.log('state in watchlistcard', state.session.watchList);
+
+  return {
+    sessionDetails: state.session,
+    isLoggedIn: state.session.isLoggedIn,
+    watchList: state.session.watchList
+  };
+};
+
+export default withRouter(connect(mapStateToProps)(WatchlistCard));
