c98e8465e9e0bde5787436f8a8157269fc789d24
MichaelMaryanoff <mmaryanoff@gmail.com>
2020-07-28 18:00:37 -0500

    Remove loading bar from persistence

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -11,7 +11,8 @@ import { loadingBarReducer } from 'react-redux-loading-bar';
 const persistConfig = {
   key: 'root',
   storage,
-  whitelist: ['session', 'form', 'loadingBar', 'spin']
+  whitelist: ['session', 'form', 'spin'],
+  blcklist: ['loadingBar']
 };
 
 const rootReducer = combineReducers({
