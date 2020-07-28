4cd85dab6221b4b3d78fa1493f45eb687bc9ccdd
MichaelMaryanoff <mmaryanoff@gmail.com>
2020-07-28 12:20:06 -0500

    Change logic for imagePath to ternary

--- src/components/WatchlistCard.js
+++ src/components/WatchlistCard.js
@@ -5,34 +5,34 @@ import reelLogoPlaceHolder from '../images/ReelLogoPlaceholder.jpg';
 
 class WatchlistCard extends React.Component {
   renderWatchList() {
-    return this.props.watchList.results.map(movie => {
-      let imageURL = `https://image.tmdb.org/t/p/original/${movie.poster_path}`;
+    if (this.props.watchList) {
+      return this.props.watchList.results.map(movie => {
+        const { id, original_title, release_date, overview, vote_average, poster_path } = movie;
 
-      if (!movie.poster_path) {
-        imageURL = reelLogoPlaceHolder;
-      }
+        let imageURL = `https://image.tmdb.org/t/p/original/${poster_path}`;
 
-      const { id, original_title, release_date, overview, vote_average } = movie;
+        let imagePath = poster_path ? imageURL : reelLogoPlaceHolder;
 
-      const modifiedOverview = `${overview.slice(0, 250)}...`;
-      const movieURL = `https://www.themoviedb.org/movie/${id}`;
+        const modifiedOverview = `${overview.slice(0, 250)}...`;
+        const movieURL = `https://www.themoviedb.org/movie/${id}`;
 
-      return (
-        <div className="ui centered card" key={`${id}`}>
-          <div className="ui image">
-            <a href={movieURL}>
-              <img src={imageURL} alt={id} />
-            </a>
+        return (
+          <div className="ui centered card" key={`${id}`}>
+            <div className="ui image">
+              <a href={movieURL}>
+                <img src={imagePath} alt={id} />
+              </a>
+            </div>
+            <div className="content">
+              <div className="header">{original_title}</div>
+              <div className="meta">Released: {release_date}</div>
+              <div className="description">{modifiedOverview}</div>
+              <div className="extra content">Average Score:{vote_average}</div>
+            </div>
           </div>
-          <div className="content">
-            <div className="header">{original_title}</div>
-            <div className="meta">Released: {release_date}</div>
-            <div className="description">{modifiedOverview}</div>
-            <div className="extra content">Average Score:{vote_average}</div>
-          </div>
-        </div>
-      );
-    });
+        );
+      });
+    }
   }
   render() {
     return <div className="ui cards">{this.renderWatchList()}</div>;
