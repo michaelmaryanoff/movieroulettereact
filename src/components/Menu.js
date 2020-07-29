4c4099be41ae8bef22b31b9d96fa94a7103e01de
MichaelMaryanoff <mmaryanoff@gmail.com>
2020-07-29 16:41:11 -0500

    User ternary for rendering header

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 
@@ -9,37 +8,15 @@ import LoggedInHeader from './LoggedInHeader';
 import LoggedOutHeader from './LoggedOutHeader';
 
 class Menu extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { headerShouldRender: true };
-  }
-
-  renderBlankHeader() {
-    return <div></div>;
-  }
-
   renderHeader() {
-    if (this.props.isLoggedIn) {
-      return (
-        <Header>
-          <LoggedInHeader />
-        </Header>
-      );
-    } else {
-      return (
-        <Header>
-          <LoggedOutHeader />
-        </Header>
-      );
-    }
+    return <Header>{this.props.isLoggedIn ? <LoggedInHeader /> : <LoggedOutHeader />}</Header>;
   }
 
   render() {
     return (
       <div>
         {this.props.location.pathname === '/login' || this.props.location.pathname === '/'
-          ? this.renderBlankHeader()
+          ? null
           : this.renderHeader()}
       </div>
     );
