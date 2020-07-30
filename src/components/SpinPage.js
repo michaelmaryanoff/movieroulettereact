dfc9066d2f995fcef718d26c6a605c42cb0e196c
MichaelMaryanoff <mmaryanoff@gmail.com>
2020-07-30 11:01:41 -0500

    Add header message

--- src/components/SpinPage.js
+++ src/components/SpinPage.js
@@ -14,6 +14,9 @@ class SpinPage extends React.Component {
   render() {
     return (
       <div className="ui container">
+        <div className="ui sizer basic centered vertical segment">
+          <div className="ui large center aligned header">Welcome to Movie Roulette!</div>
+        </div>
         <div className="ui basic segment">
           <div className="ui center aligned segment">
             <div className="ui two column stackable center aligned grid">
