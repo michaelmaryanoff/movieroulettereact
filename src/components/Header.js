d764bf95290b60801e71765b5d29f199cdd433e1
MichaelMaryanoff <mmaryanoff@gmail.com>
2020-07-31 17:27:10 -0500

    Turn header into functional component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import { connect } from 'react-redux';
 
-class Header extends React.Component {
-  render() {
-    return (
-      <div className="ui secondary pointing menu">
-        <div className="ui container">{this.props.children}</div>
-      </div>
-    );
-  }
-}
+const Header = props => {
+  return (
+    <div className="ui secondary pointing menu">
+      <div className="ui container">{props.children}</div>
+    </div>
+  );
+};
 
-const mapStateToProps = state => ({});
-
-export default connect(mapStateToProps)(Header);
+export default Header;
