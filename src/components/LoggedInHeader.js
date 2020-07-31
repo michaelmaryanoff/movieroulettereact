fa4c1aa46336fc04d09b7ccc0133f128a21531e4
MichaelMaryanoff <mmaryanoff@gmail.com>
2020-07-31 17:52:22 -0500

    Turn LoggedInHeader into functional component

--- src/components/LoggedInHeader.js
+++ src/components/LoggedInHeader.js
@@ -5,32 +5,30 @@ import { withRouter } from 'react-router';
 
 import { signOut } from '../actions';
 
-class LoggedInHeader extends React.Component {
-  handleLogout = () => {
-    this.props.signOut();
+const LoggedInHeader = props => {
+  const handleLogout = () => {
+    props.signOut();
   };
-  render() {
-    
-    return (
-      <Fragment>
-        <div>
-          <Link to="/watchlist" className="header item">
-            WATCHLIST
-          </Link>
-        </div>
-        <div>
-          <Link to="/spin" className="header item">
-            SPIN
-          </Link>
-        </div>
-        <div className="right menu">
-          <button className="ui button item" onClick={this.handleLogout}>
-            LOGOUT
-          </button>
-        </div>
-      </Fragment>
-    );
-  }
-}
+
+  return (
+    <Fragment>
+      <div>
+        <Link to="/watchlist" className="header item">
+          WATCHLIST
+        </Link>
+      </div>
+      <div>
+        <Link to="/spin" className="header item">
+          SPIN
+        </Link>
+      </div>
+      <div className="right menu">
+        <button className="ui button item" onClick={handleLogout}>
+          LOGOUT
+        </button>
+      </div>
+    </Fragment>
+  );
+};
 
 export default withRouter(connect(null, { signOut })(LoggedInHeader));
