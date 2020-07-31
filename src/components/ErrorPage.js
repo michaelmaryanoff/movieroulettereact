40db85bdc9d9d782722fbd9ef8bb8d71b158b7f8
MichaelMaryanoff <mmaryanoff@gmail.com>
2020-07-31 17:24:49 -0500

    Turn error page into functional component

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ErrorImage404 from '../images/ErrorImage404.jpg';
 
-class ErrorPage extends React.Component {
-  render() {
-    return (
-      <div>
-        <img className="ui fluid image" src={ErrorImage404} alt="Page not found" />
-      </div>
-    );
-  }
-}
+const ErrorPage = () => {
+  return (
+    <div>
+      <img className="ui fluid image" src={ErrorImage404} alt="Page not found" />
+    </div>
+  );
+};
 
 export default ErrorPage;
