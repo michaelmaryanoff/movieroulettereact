78ea5c68a3e215dcf05bb7dc8dccdf4707aa47be
MichaelMaryanoff <mmaryanoff@gmail.com>
2020-07-31 17:48:10 -0500

    Turn LoggedOutHeader into funcitonal component

--- src/components/LoggedOutHeader.js
+++ src/components/LoggedOutHeader.js
@@ -1,32 +1,26 @@
 import React, { Fragment } from 'react';
-import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-class LoggedOutHeader extends React.Component {
-  render() {
-    console.log('loggedoutheader');
-    return (
-      <Fragment>
-        <div>
-          <Link to="/login" className="header item">
-            LOG IN TO SEE WATCHLIST
-          </Link>
-        </div>
-        <div>
-          <Link to="/spin" className="header item">
-            SPIN
-          </Link>
-        </div>
-        <div className="right menu">
-          <Link to="/login" className="header item">
-            LOG IN
-          </Link>
-        </div>
-      </Fragment>
-    );
-  }
-}
+const LoggedOutHeader = () => {
+  return (
+    <Fragment>
+      <div>
+        <Link to="/login" className="header item">
+          LOG IN TO SEE WATCHLIST
+        </Link>
+      </div>
+      <div>
+        <Link to="/spin" className="header item">
+          SPIN
+        </Link>
+      </div>
+      <div className="right menu">
+        <Link to="/login" className="header item">
+          LOG IN
+        </Link>
+      </div>
+    </Fragment>
+  );
+};
 
-const mapStateToProps = state => ({});
-
-export default connect(mapStateToProps)(LoggedOutHeader);
+export default LoggedOutHeader;
