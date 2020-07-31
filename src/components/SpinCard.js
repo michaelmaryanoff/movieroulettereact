079bf4bfb75969997505fad510a89adb9513cf03
MichaelMaryanoff <mmaryanoff@gmail.com>
2020-07-31 18:20:08 -0500

    Break spin results card into component

--- src/components/SpinCard.js
+++ src/components/SpinCard.js
@@ -4,9 +4,11 @@ import { connect } from 'react-redux';
 import { addToWatchlist, resetWatchlistUpdateStatus, spinningCompleted } from '../actions';
 
 import reelLogoPlaceHolder from '../images/ReelLogoPlaceholder.jpg';
+
 import WatchlistAddButton from './WatchlistAddButton';
 import LoadingCard from './LoadingCard';
 import NoResultsCard from './NoResultsCard';
+import SpinResultCard from './SpinResultCard';
 
 export class SpinCard extends Component {
   componentDidMount() {
@@ -28,51 +30,7 @@ export class SpinCard extends Component {
     }
 
     if (this.props.selectedMovie) {
-      let {
-        poster_path,
-        id,
-        original_title,
-        release_date,
-        overview,
-        vote_average
-      } = this.props.selectedMovie;
-
-      let modifiedOverview = `${overview.slice(0, 250)}...`;
-
-      let imageURL = `https://image.tmdb.org/t/p/original/${poster_path}`;
-      const movieURL = `https://www.themoviedb.org/movie/${id}`;
-
-      if (!this.props.selectedMovie.poster_path) {
-        imageURL = reelLogoPlaceHolder;
-      }
-      return (
-        <div className="ui card centered" key={id}>
-          <div className="content">
-            <h3>
-              <em>Tonight you're watching...</em>
-            </h3>
-            <h3 className="header">{original_title}</h3>
-            <div className="meta">Released: {release_date}</div>
-            <p />
-            <div className="ui centered small image">
-              <a href={movieURL}>
-                <img src={imageURL} alt={id} />
-              </a>
-            </div>
-
-            <div className="description">
-              <p />
-              <p />
-              {modifiedOverview}
-            </div>
-            <div className="extra content">Average Score: {vote_average}</div>
-            <p />
-            <div>
-              <WatchlistAddButton handleAdd={this.handleAddToWatchlist} />
-            </div>
-          </div>
-        </div>
-      );
+      return <SpinResultCard />;
     }
   }
 
---
+++ src/components/SpinResultCard.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import WatchlistAddButton from './WatchlistAddButton';
+import { connect } from 'react-redux';
+import { addToWatchlist } from '../actions';
+
+import reelLogoPlaceHolder from '../images/ReelLogoPlaceholder.jpg';
+
+class SpinResultCard extends Component {
+  handleAddToWatchlist = event => {
+    event.preventDefault();
+
+    this.props.addToWatchlist(this.props.selectedMovie.id);
+  };
+  renderCard() {
+    let {
+      poster_path,
+      id,
+      original_title,
+      release_date,
+      overview,
+      vote_average
+    } = this.props.selectedMovie;
+
+    let modifiedOverview = `${overview.slice(0, 250)}...`;
+
+    let imageURL = `https://image.tmdb.org/t/p/original/${poster_path}`;
+    const movieURL = `https://www.themoviedb.org/movie/${id}`;
+
+    if (!this.props.selectedMovie.poster_path) {
+      imageURL = reelLogoPlaceHolder;
+    }
+
+    return (
+      <div className="ui card centered" key={id}>
+        <div className="content">
+          <h3>
+            <em>Tonight you're watching...</em>
+          </h3>
+          <h3 className="header">{original_title}</h3>
+          <div className="meta">Released: {release_date}</div>
+          <p />
+          <div className="ui centered small image">
+            <a href={movieURL}>
+              <img src={imageURL} alt={id} />
+            </a>
+          </div>
+
+          <div className="description">
+            <p />
+            <p />
+            {modifiedOverview}
+          </div>
+          <div className="extra content">Average Score: {vote_average}</div>
+          <p />
+          <div>
+            <WatchlistAddButton handleAdd={this.handleAddToWatchlist} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+  render() {
+    return <div>{this.renderCard()}</div>;
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    selectedMovie: state.spin.selectedMovie
+  };
+};
+
+export default connect(mapStateToProps, { addToWatchlist })(SpinResultCard);
