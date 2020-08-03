b5e5ca99aa599315609e9044ec6294035a1826f6
MichaelMaryanoff <mmaryanoff@gmail.com>
2020-08-03 16:25:39 -0500

    Create a conditional render of add to watchlist button

---
+++ src/components/GuestWatchListAddButton.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const GuestWatchListAddButton = () => {
+  return (
+    <div>
+      <button disabled={true} className="ui fluid large inactive submit button">
+        Log in to add to Watchlist
+      </button>
+    </div>
+  );
+};
+
+export default GuestWatchListAddButton;
--- src/components/WatchlistAddButton.js
+++ src/components/WatchlistAddButton.js
@@ -1,30 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import AuthedWatchListAddButton from './AuthedWatchListAddButton';
+import GuestWatchListAddButton from './GuestWatchListAddButton';
 
 class WatchlistAddButton extends React.Component {
-  renderButton() {
-    if (!this.props.isLoggedIn) {
-      return (
-        <div>
-          <button disabled={true} className="ui fluid large inactive submit button">
-            Log in to add to Watchlist
-          </button>
-        </div>
-      );
-    } else {
-      return <AuthedWatchListAddButton />;
-    }
-  }
   render() {
-    return <div>{this.renderButton()}</div>;
+    return (
+      <div>
+        {this.props.isLoggedIn ? <AuthedWatchListAddButton /> : <GuestWatchListAddButton />}
+      </div>
+    );
   }
 }
 
 const mapStateToProps = state => {
   return {
-    isLoggedIn: state.session.isLoggedIn,
-    isWatchListUpdated: state.spin.isWatchListUpdated
+    isLoggedIn: state.session.isLoggedIn
   };
 };
 
