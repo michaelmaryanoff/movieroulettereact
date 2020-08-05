35ce2e095e8608428c9dbe88946f4c41d30ba52e
MichaelMaryanoff <mmaryanoff@gmail.com>
2020-08-05 10:07:34 -0500

    Add github icon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,10 @@ const Footer = () => {
   return (
     <footer className="ui vertical  footer segment">
       <div className="ui center aligned container">
+        <a style={{ color: 'black' }} href="https://github.com/michaelmaryanoff">
+          <i href="https://github.com/michaelmaryanoff" className="github link big icon" />
+        </a>
+        <p />
         <em>This product uses the TMDb API but is not endorsed or certified by TMDb.</em>
         <p></p>
         <img src={tmdbLogo} alt="tmdb logo" className="ui centered tiny image"></img>
