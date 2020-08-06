618855a8c9302d22558d885bdda0f119ddfe6dca
MichaelMaryanoff <mmaryanoff@gmail.com>
2020-08-06 17:06:49 -0500

    Fix but where watchlist was not updating

--- src/components/Watchlist.js
+++ src/components/Watchlist.js
@@ -2,11 +2,18 @@ import React from 'react';
 import { withRouter } from 'react-router';
 import { connect } from 'react-redux';
 
+import { getWatchList, updatingWatchListCompleted } from '../actions';
+
 import WatchlistCards from './WatchlistCards';
 import WatchlistLoginError from './WatchlistLoginError';
 import Footer from './Footer';
 
 class Watchlist extends React.Component {
+  componentDidMount() {
+    this.props.getWatchList();
+
+    this.props.updatingWatchListCompleted();
+  }
   render() {
     return (
       <div className="ui container">
@@ -25,4 +32,6 @@ const mapStateToProps = state => {
   };
 };
 
-export default withRouter(connect(mapStateToProps)(Watchlist));
+export default withRouter(
+  connect(mapStateToProps, { getWatchList, updatingWatchListCompleted })(Watchlist)
+);
