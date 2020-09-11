83c46afa7538ea7d1e13be664d647af4fcfc47fb
MichaelMaryanoff <mmaryanoff@gmail.com>
2020-09-11 17:04:36 -0500

    Get code into state on select

--- src/components/SpinForm.js
+++ src/components/SpinForm.js
@@ -20,29 +20,6 @@ import Dropdown from './Dropdown';
 import DropdownOptions from './DropdownOptions';
 import GenreDropDownOptionList from './GenreDropDownOptionList';
 
-const languageOptions = [
-  { key: 'Arabic', text: 'drabic', value: 'Arabic' },
-  { key: 'Chinese', text: 'dhinese', value: 'Chinese' },
-  { key: 'Danish', text: 'danish', value: 'Danish' },
-  { key: 'Dutch', text: 'Dutch', value: 'Dutch' },
-  { key: 'English', text: 'English', value: 'English' },
-  { key: 'French', text: 'French', value: 'French' },
-  { key: 'German', text: 'German', value: 'German' },
-  { key: 'Greek', text: 'Greek', value: 'Greek' },
-  { key: 'Hungarian', text: 'Hungarian', value: 'Hungarian' },
-  { key: 'Italian', text: 'Italian', value: 'Italian' },
-  { key: 'Japanese', text: 'Japanese', value: 'Japanese' },
-  { key: 'Korean', text: 'Korean', value: 'Korean' },
-  { key: 'Lithuanian', text: 'Lithuanian', value: 'Lithuanian' },
-  { key: 'Persian', text: 'Persian', value: 'Persian' },
-  { key: 'Polish', text: 'Polish', value: 'Polish' },
-  { key: 'Portuguese', text: 'Portuguese', value: 'Portuguese' },
-  { key: 'Russian', text: 'Russian', value: 'Russian' },
-  { key: 'Spanish', text: 'Spanish', value: 'Spanish' },
-  { key: 'Swedish', text: 'Swedish', value: 'Swedish' },
-  { key: 'Turkish', text: 'Turkish', value: 'Turkish' },
-  { key: 'Vietnamese', text: 'Vietnamese', value: 'Vietnamese' }
-];
 class SpinForm extends React.Component {
   constructor(props) {
     super(props);
@@ -76,9 +53,9 @@ class SpinForm extends React.Component {
       // The code of the currently selected genre (used for our network request)
       genreCode: '',
 
-      displayLanguage: '',
+      languageArray,
 
-      languageArray
+      languageCode: ''
     };
   }
 
@@ -137,7 +114,8 @@ class SpinForm extends React.Component {
   };
 
   handleSemanticDropDownChange = (event, data) => {
-    console.log(data.value);
+
+    this.setState({ languageCode: data.value });
   };
 
   renderSpinForm() {
